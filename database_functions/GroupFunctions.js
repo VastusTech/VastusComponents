b4acb6ee604abd014d1cc2e1808b1befc7de2bc8
Leo Belyi <leonid@ac93.org>
2019-02-05 15:06:25 -0500

    Fix creating groups and clean up the create group comp

--- database_functions/GroupFunctions.js
+++ database_functions/GroupFunctions.js
@@ -1,5 +1,6 @@
 import Lambda from "../api/Lambda";
 import UserFunctions from "./UserFunctions";
+import S3 from "../api/S3Storage";
 
 const itemType = "Group";
 
@@ -7,11 +8,11 @@ class GroupFunctions {
     // TODO THESE ARE THE HIGH-LEVEL DATABASE ACTION FUNCTIONS
     // =============================================================================
     // Create Functions ============================================================
-    static createGroup(fromID, title, description, successHandler, failureHandler) {
-        return this.create(fromID, title, description, null, null, null, null, null, successHandler, failureHandler);
+    static createGroup(fromID, title, description, access, successHandler, failureHandler) {
+        return this.create(fromID, title, description, null, null, access, null, null, null, null, successHandler, failureHandler);
     }
-    static createGroupOptional(fromID, title, description, access, restriction, owners, members, tags, successHandler, failureHandler) {
-        return this.create(fromID, title, description, access, restriction, owners, members, tags, successHandler, failureHandler);
+    static createGroupOptional(fromID, title, description, motto, groupImage, access, restriction, owners, members, tags, successHandler, failureHandler) {
+        return this.create(fromID, title, description, motto, groupImage, access, restriction, owners, members, tags, successHandler, failureHandler);
     }
 
     // Update Functions ============================================================
@@ -48,16 +49,26 @@ class GroupFunctions {
 
     // TODO THESE ARE THE LOW-LEVEL DATABASE ACTION FUNCTIONS
     // =============================================================================
-    static create(fromID, title, description, access, restriction, owners, members, tags, successHandler, failureHandler) {
+    static create(fromID, title, description, motto, groupImage, access, restriction, owners, members, tags, successHandler, failureHandler) {
         return Lambda.create(fromID, itemType, {
             title,
             description,
+            motto,
             access,
             restriction,
             owners,
             members,
             tags,
-        }, successHandler, failureHandler);
+            groupImagePath: (groupImage) ? "groupImage" : null,
+        }, (data) => {
+            if (groupImage) {}
+            const id = data.data;
+            // Put in the group image
+            S3.putImage(id + "/groupImage", groupImage, successHandler, (error) => {
+                // Best effort to delete!
+                GroupFunctions.delete(fromID, id);
+            });
+        }, failureHandler);
     }
     static updateAdd(fromID, eventID, attributeName, attributeValue, successHandler, failureHandler) {
         return Lambda.updateAddToAttribute(fromID, eventID, itemType, attributeName, attributeValue, successHandler, failureHandler);
