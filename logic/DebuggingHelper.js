1051eba2c9b261d10fa91312c7c73dcba7f06115
Leo Belyi <leonid@ac93.org>
2019-03-06 13:35:29 -0500

    Fix message board minimum error

--- logic/DebuggingHelper.js
+++ logic/DebuggingHelper.js
@@ -15,3 +15,16 @@ export function debugAlert(message) {
         alert(message);
     }
 }
+export function getMethods(obj) {
+    const result = [];
+    for (let id in obj) {
+        try {
+            if (typeof(obj[id]) === "function") {
+                result.push(id + ": " + obj[id].toString());
+            }
+        } catch (err) {
+            result.push(id + ": inaccessible");
+        }
+    }
+    return result;
+}
