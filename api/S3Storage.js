feeebaf5e95b20f0e5bd19d96b645800eb87221f
Leo Belyi <leonid@ac93.org>
2019-04-12 10:15:05 -0400

    Document S3 storage module

--- api/S3Storage.js
+++ api/S3Storage.js
@@ -7,85 +7,62 @@ import {err, log} from "../../Constants";
  */
 class S3Storage {
     static bucketName = 'vastusofficial';
-    // User-Level Functions
+    static multipartSize = 10 * 1024 * 1024;
+    static multipartQueueSize = 8;
+
+    // ======================================================================================================
+    // S3 Storage High-Level Functions ~
+    // ======================================================================================================
 
     /**
-     * TODO
+     * Puts an image into the S3 bucket at a specific path (using file structure syntax).
      *
-     * @param path
-     * @param image
-     * @param successHandler
-     * @param failureHandler
+     * @param {string} path The key path for the file.
+     * @param {*} image The file to place into the S3 bucket.
+     * @param {function(*)} successHandler The function to handle the result of the S3 placement.
+     * @param {function(error)} failureHandler The function to handle any errors that may occur.
+     * @return {*} Debugging info about the S3 operation.
      */
     static putImage(path, image, successHandler, failureHandler) {
-        S3Storage.put(path, image, "image/*", successHandler, failureHandler);
+        return S3Storage.put(path, image, "image/*", progress => console.log(progress.loaded + "/" + progress.total),
+            successHandler, failureHandler);
     }
 
     /**
-     * TODO
+     * Puts a video into the S3 bucket at a specific path (using file structure syntax).
      *
-     * @param path
-     * @param video
-     * @param successHandler
-     * @param failureHandler
+     * @param {string} path The key path for the file.
+     * @param {*} video The file to place into the S3 bucket.
+     * @param {function(*)} successHandler The function to handle the result of the S3 placement.
+     * @param {function(error)} failureHandler The function to handle any errors that may occur.
+     * @return {*} Debugging info about the S3 operation.
      */
     static putVideo(path, video, successHandler, failureHandler) {
-        let bucket = new AWS.S3({params: {Bucket: S3Storage.bucketName}});
-        if (video) {
-            let params = {Key: "public/" + path, ContentType: video.type, Body: video};
-            let options = {partSize: 10 * 1024 * 1024, queueSize: 8};
-            bucket.upload(params, options).on('httpUploadProgress', function(evt) {
-                console.log("Uploaded :: " + parseInt((evt.loaded * 100) / evt.total )+'%');
-            }).send(function(error, data) {
-                if (error) {
-                    err&&console.error(error);
-                    failureHandler(err);
-                }
-                else {
-                    successHandler(data);
-                    log&&console.log("Video uploaded successfully.");
-                }
-            });
-        }
+        return S3Storage.multipartPut(path, video, "video/*", progress => console.log("Uploaded :: " +
+            parseInt((progress.loaded * 100) / progress.total )+'%'), successHandler, failureHandler);
     }
 
     /**
-     * TODO
+     * Puts either a picture or a video into the S3 bucket at a specific path (using file structure syntax).
      *
-     * @param path
-     * @param media
-     * @param successHandler
-     * @param failureHandler
+     * @param {string} path The key path for the file.
+     * @param {*} media The file to place into the S3 bucket.
+     * @param {function(*)} successHandler The function to handle the result of the S3 placement.
+     * @param {function(error)} failureHandler The function to handle any errors that may occur.
+     * @return {*} Debugging info about the S3 operation.
      */
     static putVideoOrImage(path, media, successHandler, failureHandler) {
-        S3Storage.put(path, media, "video/*;image/*", successHandler, failureHandler);
-    }
-
-    // Low Level Functions
-    /**
-     * TODO
-     *
-     * @param path
-     * @param successHandler
-     * @param failureHandler
-     */
-    static get(path, successHandler, failureHandler) {
-        // returns URL
-        Storage.get(path).then((url) => {
-            log&&console.log("Storage successfully retrieved file! URL = " + url);
-            if (successHandler) { successHandler(url); }
-        }).catch((error) => {
-            err&&console.error("Storage failed to retrieve file with path = " + path + "... Error: " + error);
-            if (failureHandler) { failureHandler(error); }
-        });
+        return S3Storage.put(path, media, "video/*;image/*", progress => console.log(progress.loaded + "/" + progress.total),
+            successHandler, failureHandler);
     }
 
     /**
-     * TODO
+     * Determines whether or not the given path actually corresponds to a file in S3 or not.
      *
-     * @param path
-     * @param successHandler
-     * @param failureHandler
+     * @param {string} path The key path for the file.
+     * @param {function(boolean)} successHandler The function to handle the answer of whether or not the path exists.
+     * @param {function(error)} failureHandler The function to handle any errors that may occur.
+     * @return {*} Debugging info about the S3 operation.
      */
     static ifExists(path, successHandler, failureHandler) {
         let bucket = new AWS.S3({params: {Bucket: S3Storage.bucketName}});
@@ -105,33 +82,92 @@ class S3Storage {
                 successHandler(true);
             }
         });
+        return null;
+    }
+
+    // ======================================================================================================
+    // S3 Storage Low-Level Functions ~
+    // ======================================================================================================
+
+    /**
+     * Gets the media from the S3 bucket for a file at a specific path.
+     *
+     * @param {string} path The key path for the file.
+     * @param {function(*)} successHandler The function to handle the successfully received file.
+     * @param {function(error)} failureHandler The function to handle any errors that may occur.
+     * @return {*} Debugging info about the S3 operation.
+     */
+    static get(path, successHandler, failureHandler) {
+        // returns URL
+        Storage.get(path).then((url) => {
+            log&&console.log("Storage successfully retrieved file! URL = " + url);
+            if (successHandler) { successHandler(url); }
+        }).catch((error) => {
+            err&&console.error("Storage failed to retrieve file with path = " + path + "... Error: " + error);
+            if (failureHandler) { failureHandler(error); }
+        });
+        return null;
     }
 
     /**
-     * TODO
+     * Puts a file into S3 using the Amplify Storage module.
      *
-     * @param path
-     * @param file
-     * @param contentType
-     * @param successHandler
-     * @param failureHandler
+     * @param {string} path The key path for the file.
+     * @param {*} file The file to place into the S3 bucket.
+     * @param {string} contentType The type of the content to place into the bucket.
+     * @param {function({loaded: number, total: number})} progressHandler
+     * @param {function(*)} successHandler The function to handle the result of the S3 placement.
+     * @param {function(error)} failureHandler The function to handle any errors that may occur.
+     * @return {*} Debugging info about the S3 operation.
      */
-    static put(path, file, contentType, successHandler, failureHandler) {
-        Storage.put(path, file, { contentType, progressCallback(progress) { console.log(`Uploaded: ${progress.loaded}/${progress.total}`);} }).then((result) => {
+    static put(path, file, contentType, progressHandler, successHandler, failureHandler) {
+        Storage.put(path, file, { contentType, progressCallback: progressHandler}).then((result) => {
             log&&console.log("Storage successfully put file in! Result: " + JSON.stringify(result));
             if (successHandler) { successHandler(result); }
         }).catch((error) => {
             err&&console.error("Storage failed put function... Error: " + error);
             if (failureHandler) { failureHandler(error); }
         });
+        return null;
     }
 
     /**
-     * TODO
+     * Uses the AWS S3 Module's Multipart put to make a put operation go faster. Uses multi-threading to speed up the
+     * upload.
      *
-     * @param path
-     * @param successHandler
-     * @param failureHandler
+     * @param {string} path The key path for the file.
+     * @param {*} file The file to place into the S3 bucket.
+     * @param {string} contentType The type of the content to place into the bucket.
+     * @param {function({loaded: number, total: number})} progressHandler
+     * @param {function(*)} successHandler The function to handle the result of the S3 placement.
+     * @param {function(error)} failureHandler The function to handle any errors that may occur.
+     * @return {*} Debugging info about the S3 operation.
+     */
+    static multipartPut(path, file, contentType, progressHandler, successHandler, failureHandler) {
+        // TODO Test if this still works generalized like this...
+        let bucket = new AWS.S3({params: {Bucket: S3Storage.bucketName}});
+        let params = {Key: "public/" + path, ContentType: contentType, Body: file};
+        let options = {partSize: S3Storage.multipartSize, queueSize: S3Storage.multipartQueueSize};
+        bucket.upload(params, options).on('httpUploadProgress', progressHandler).send((error, data) => {
+            if (error) {
+                err&&console.error(error);
+                failureHandler(err);
+            }
+            else {
+                successHandler(data);
+                log&&console.log("Video uploaded successfully.");
+            }
+        });
+        return null;
+    }
+
+    /**
+     * Deletes a file from the S3 bucket at the given path.
+     *
+     * @param {string} path The key path of the file.
+     * @param {function(*)} successHandler The function to handle the result of the successful deletion.
+     * @param {function(error)} failureHandler The function to handle any errors that may occur.
+     * @return {*} Debugging info about the S3 operation.
      */
     static delete(path, successHandler, failureHandler) {
         Storage.remove(path).then((result) => {
@@ -141,6 +177,7 @@ class S3Storage {
             err&&console.error("Storage failed remove function... Error: " + error);
             if (failureHandler) { failureHandler(error); }
         });
+        return null;
     }
 }
 
